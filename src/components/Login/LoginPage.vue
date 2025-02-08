f8350bd9d15a8de5a6a762324ae9087bbc89bb73
Mashu_Official <13986433+mashu_official@user.noreply.gitee.com>
2025-02-08 15:48:29 +0800

    基本布局 0.4.0 登录页面开工

--- src/components/Login/LoginPage.vue
+++ src/components/Login/LoginPage.vue
@@ -2,32 +2,28 @@
 
 
     <div class="flex flex-row w-full h-full items-start overflow-hidden">
-        <div class="flex-1  h-full" :style="backgroundImageStyle">
+        <div class="flex-1  h-full" :style="backgroundImageStyle"
+             style="
+                background-repeat: no-repeat;
+                background-position: top;
+                background-size: cover;">
 
 
         </div>
         <div class="flex flex-col flex-1 items-center justify-center">
-            123123
+
         </div>
     </div>
-    <!--    <div class="flex flex-row h-full w-full">-->
-    <!--        <div class="flex-1 flex-shrink-0 h-full overflow-hidden"> &lt;!&ndash; 添加 overflow-hidden 来防止内容溢出 &ndash;&gt;-->
 
-    <!--        </div>-->
-    <!--        <div class="flex flex-row flex-1 items-center justify-center">-->
-    <!--123-->
-    <!--        </div>-->
-    <!--    </div>-->
 </template>
 
 <script setup lang="ts">
 import RoomSidebar from "../Contents/Room/RoomSidebar.vue";
 import RoomContent from "../Contents/Room/RoomContent.vue";
 import {computed} from "vue";
+
 const backgroundImageStyle = computed(() => ({
-    backgroundImage: `url('/LoginPage/LoginCoverTomori.png')`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'top',
+    backgroundImage: `url('/LoginPage/LoginCoverTomori.png')`
 }));
 </script>
 
